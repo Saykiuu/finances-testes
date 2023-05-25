db4b8eda25ed39f6440aa64216ae53be0d00a1f5
David Pontes <88503892+Saykiuu@users.noreply.github.com>
2023-05-25 17:04:10 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
 
-  LOCALAPI: 'http://localhost:20080/'
+  // LOCALAPI: 'http://localhost:20080/'
+  LOCALAPI: 'https://church-control-back.onrender.com/'
+
 };
